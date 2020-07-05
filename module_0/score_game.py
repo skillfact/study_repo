b674075272f53831691b8c965614c970579a7d52
skillfact <skillfact3@gmail.com>
2020-07-05 11:47:00 +0300

    variables in functions

--- module_0/score_game.py
+++ module_0/score_game.py
@@ -1,14 +1,19 @@
 import numpy as np
         
         
-def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+def score_game(game_core, start=1, end=100, quant=1000):
+    '''Запускаем игру quant раз, чтобы узнать, как быстро игра угадывает число'''
+    # или можно добавить input-ы
+    if start > end:
+        print('Ошибка ввода интервала')
+        return
+        
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1,101, size=(1000))
+    random_array = np.random.randint(start,end+1, size=(quant))
     
     for number in random_array:
-        count_ls.append(game_core(number))
+        count_ls.append(game_core(number, start, end))
     
     score = int(np.mean(count_ls))
     
@@ -16,12 +21,12 @@ def score_game(game_core):
     return(score)
 
 
-def game_core_v3(number):
+def game_core_v3(number, start, end):
     '''Сначала устанвим интервал в половину возможных чисел, потом уменьшаем интервал в 2 раза пока не полчим искомое число.
        Функция принимает загаданное число и возвращает число попыток'''
     count = 1
-    predict_high = 50
-    predict_low = 1  
+    predict_high = int(end/2)
+    predict_low = start  
     tmp = 0
     
     while number != predict_high and number != predict_low:
@@ -37,4 +42,4 @@ def game_core_v3(number):
     return(count) # выход из цикла, если угадали
 
             
-score_game(game_core_v3)
+score_game(game_core_v3,1,100,1000)
